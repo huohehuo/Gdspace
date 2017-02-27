28db9833df335a29f7d13875b2302def18d333d8
cyber <753392431@qq.com>
2017-02-27 14:58:34 +0800

    定位，Marker的使用，自定义，拖动

---
+++ app/src/main/java/com/example/lins/gdspace/base/App.java
@@ -0,0 +1,14 @@
+package com.example.lins.gdspace.base;
+
+import android.app.Application;
+
+/**
+ * Created by LINS on 2017/2/23.
+ */
+
+public class App extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/example/lins/gdspace/service/Service.java
@@ -0,0 +1,8 @@
+package com.example.lins.gdspace.service;
+
+/**
+ * Created by LINS on 2017/2/23.
+ */
+
+public class Service {
+}
